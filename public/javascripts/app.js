1b93f0e30ec2fb79df52ca017247bbbb08d61d5f
jleewebdev <lee45276@gmail.com>
2016-03-02 19:08:46 +0800

    Added AlbumsView, render all albums to home page

---
+++ public/javascripts/app.js
@@ -0,0 +1,18 @@
+var App = {
+  albumsLoaded: function() {
+    this.view.render();
+  },
+
+
+  fetchAlbums: function() {
+    this.albums = new Albums();
+    this.view = new AlbumsView({ collection: this.albums })
+    this.albums.fetch({
+      success: this.albumsLoaded.bind(this)
+    });
+  },
+
+  init: function() {
+    this.fetchAlbums();
+  }
+};
