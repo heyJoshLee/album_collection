0314d0891c1efd690d1727b24b2ec025bdd9d1dd
jleewebdev <lee45276@gmail.com>
2016-03-03 21:21:56 +0800

    Added track model, view and collection

---
+++ public/javascripts/views/tracks.js
@@ -0,0 +1,36 @@
+var $overlay = $("#overlay");
+
+var TracksView = Backbone.View.extend({
+    duration: 300,
+    tempate: Handlebars.compile($("[data-name='tracks']").html()),
+
+    open: function() {
+      this.$el.add($overlay).fadeIn(300);
+    },
+
+    close: function(e) {
+      e.preventDefault();
+      this.fadeOut();
+    },
+
+    fadeOut: function() {
+      $overlay.fadeOut(this.duration);
+      this.$el.fadeOut(this.duration, function() {
+        this.remove();
+      }.bind(this));
+    },
+
+    render: function() {
+      this.$el.html(this.template({
+        tracks: this.collection.toJSON(I),
+        album: this.album
+      }));
+      this.open();
+    },
+
+
+    initialize: function(options) {
+      this.album = options.album;
+      this.$el.appendTo(document.body);
+    }
+});
